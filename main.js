f0b45bd836f130e5dfad1fdd66d6e0475af74cd3
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-14 14:33:33 -0600

    added spinner

--- main.js
+++ main.js
@@ -262,7 +262,7 @@ for (const button of buttons) {
 function loading() {
     let spinner =
         `<div class="text-center">
-  <div class="spinner-border" role="status">
+  <div class="spinner-border text-light" role="status">
     <span class="sr-only">Loading...</span>
   </div>
 </div>`
